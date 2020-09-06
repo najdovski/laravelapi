d7dfb3709fd644547303a4da85e3c55b7f3bc77c
Kristijan Najdovski <kristijannds@gmail.com>
2020-09-06 12:48:52 +0200

    API Crud finished

--- database/migrations/2020_09_06_084633_create_articles_table.php
+++ database/migrations/2020_09_06_084633_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->test('body');
+            $table->text('body');
             $table->timestamps();
         });
     }
